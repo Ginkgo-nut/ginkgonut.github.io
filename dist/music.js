5a5b0801f95cccdfdb2463887c16ec72e28f8ac8
Zengyayi <yyzengk@isoftstone.com>
2023-03-18 10:35:21 +0800

    Site updated: 2023-03-18 10:35:20

--- dist/music.js
+++ dist/music.js
@@ -12,7 +12,7 @@ const ap = new APlayer({
         url: "http://music.163.com/song/media/outer/url?id=2019573476.mp3",
         cover:
           "http://p1.music.126.net/4x-65bnidSKyEMDBmGtn8g==/109951168466764238.jpg?param=130y130",
-        lrc: "[00:00.000] 作词 : 林可邦\n[00:01.000] 作曲 : 蔡健雅",
+        // lrc: "[00:00.000] 作词 : 林可邦\n[00:01.000] 作曲 : 蔡健雅",
       },
     ],
   });
