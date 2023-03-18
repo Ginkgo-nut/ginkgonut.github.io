9095b70d1c8bf095912fb8918734efd06bf4eec7
Zengyayi <yyzengk@isoftstone.com>
2023-03-18 22:34:23 +0800

    Site updated: 2023-03-18 22:34:22

--- dist/music.js
+++ dist/music.js
@@ -1,3 +1,11 @@
+/*
+ * @Author: Zengyayi yyzengk@isoftstone.com
+ * @Date: 2023-03-16 22:33:13
+ * @LastEditors: Zengyayi yyzengk@isoftstone.com
+ * @LastEditTime: 2023-03-18 22:12:22
+ * @FilePath: \blog\themes\next-eight\source\dist\music.js
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
 const ap = new APlayer({
     container: document.getElementById("aplayer"),
     fixed: true,
